56b5d2e2d92e9a3658c14a3de7f66a58b413ee22
Nilesh Balotiya <97271873+Nilesh270@users.noreply.github.com>
2026-07-29 12:49:49 +0530

    Codeforces: 133A - HQ9+

---
+++ codeforces/133a/133a.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws Exception {
+
+        int t = 1;
+
+//        t = fs.nextInt();
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        out.flush();
+    }
+
+    static void solve() throws Exception {
+        String s = fs.next();
+        if(s.contains("H") || s.contains("Q") || s.contains("9")){
+            out.println("YES");
+        }else{
+            out.println("NO");
+        }
+    }
+
+
+
+    // ===================== Fast Scanner =====================
+
+    static class FastScanner {
+
+        private final BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+    }
+
+}
---
+++ codeforces/133a/README.md
@@ -0,0 +1,3 @@
+# 133A - HQ9+
+
+**Link:** [https://codeforces.com/contest/133/problem/A](https://codeforces.com/contest/133/problem/A)
